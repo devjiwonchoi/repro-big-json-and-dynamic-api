3b5828c37d1be9a2f9c3069524f82721f1281259
devjiwonchoi <devjiwonchoi@gmail.com>
2025-11-14 09:46:02 +0100

    simplify using connection

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from 'react'
+
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Suspense>{children}</Suspense>
+      </body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,8 @@
-import { Suspense } from 'react'
-import { cookies } from 'next/headers'
 import { getCourses } from './get-course'
+import { connection } from 'next/server'
 
 export default async function Page() {
   const courses = getCourses()
-  return (
-    <>
-      <Suspense fallback={<p>Loading...</p>}>
-        <DynamicAPI />
-      </Suspense>
-      {courses.map((course) => (
-        <div key={course._id}>{course.title}</div>
-      ))}
-    </>
-  )
-}
-async function DynamicAPI() {
-  // dynamic APIs like cookies, headers, etc.
-  // Remove me to see the issue go away.
-  await cookies()
-
-  return <p>hello world</p>
+  await connection()
+  return courses.map((course) => <div key={course._id}>{course.title}</div>)
 }
